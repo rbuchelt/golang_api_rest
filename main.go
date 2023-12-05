463bc2e8b82e64a9e6c163b4e4eaa8d92f1e063b
renan.oliveira <rebuchelt1@gmail.com>
2023-12-05 10:06:14 -0300

    primeiro commit projeto golang-api

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"api-rest/models"
+	"api-rest/routes"
+	"fmt"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	fmt.Println("Iniciando o servidor Rest com GO")
+	routes.HandleRequest()
+}
